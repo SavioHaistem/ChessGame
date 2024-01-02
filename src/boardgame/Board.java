027d9a9bb1737136cc0176b29d3d1fbe6ed5b51e
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-02 09:49:26 -0300

    Create BoardExceptions

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer rows, Integer columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("error ao criar board: e necessario que hajda pelomenos 1 linha e uma coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
@@ -15,30 +18,43 @@ public class Board {
         return columns;
     }
 
-    public void setColumns(Integer columns) {
-        this.columns = columns;
-    }
-
     public Integer getRows() {
         return rows;
     }
 
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
-
     public Piece piece(int row, int column) {
+        if(!positionExists(row, column)) {
+            throw new BoardException("Posicao nao existe");
+        }
         return pieces[row][column];
+
     }
 
     public Piece piece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("Posicao nao existe");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("A posiçao ja esta ocupada");
+        }
         this.pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
-
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    };
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(),position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("A posiçao nao existe");
+        }
+        return piece(position) != null;
+    }
 }
